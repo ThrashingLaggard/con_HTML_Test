ddfe52da7a3723e0651cd1cdd75f9d25ae4e64fc
Gusteltrude <Test@mail.lol>
2024-09-19 20:08:14 +0200

    Letzter Commit vor dem Merge

--- con_HTML_Test/obj/Debug/net8.0/con_HTML_Test.AssemblyInfo.cs
+++ con_HTML_Test/obj/Debug/net8.0/con_HTML_Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("con_HTML_Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4439f2c739e69f4c56aef05b04c41d3676ac2521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a0458a73f444e32958108699b9dc6030b3ba7fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("con_HTML_Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("con_HTML_Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
